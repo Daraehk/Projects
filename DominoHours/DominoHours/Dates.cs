fa05eeee272986e4f4352b1cf3af4b96f1c264a2
Denes <denesone@msn.com>
2018-10-25 22:07:09 +0100

    Work hours
    My girlfriends works at the Domino's, and she used to write down her hours on little piece of papers. I made her this app, so she can save digitally her hours, and the program calculates some estimates and statistics about the last 4 week period. (WPF)

---
+++ DominoHours/DominoHours/Dates.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DominoHours
+{
+    public class Dates
+    {
+        //Members
+        private DateTime _date;
+        public string Date
+        {
+            get { return _date.ToShortDateString(); }
+            set { DateTime.TryParse(value, out _date); }
+        }
+        private DateTime _from;
+        public string From
+        {
+            get {  return _from.ToShortTimeString(); }
+            set {  DateTime.TryParse(value, out _from); }
+        }
+        private DateTime _to;
+        public string To
+        {
+            get { return _to.ToShortTimeString(); }
+            set { DateTime.TryParse(value, out _to); }
+        }
+        private TimeSpan _workedHours;
+        public string Worked
+        {
+            get { return _to.Subtract(_from).ToString(@"hh\:mm"); }
+            set { TimeSpan.TryParse(value, out _workedHours); }
+        }
+        public static List<Dates> DateList { get; set; } = new List<Dates>();
+
+        //Get file paths
+        public static string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).ToString() + "worked_hours.doc";
+        public static string path_Statistics = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).ToString() + "Monthly_Statistics.doc";
+
+        //Methods
+
+        public static void AddToList(List<Dates> dates, string From, string To)
+        {
+            //Get values from DateTime.Today() and source          
+            Dates Today = new Dates();
+            {
+                Today.Date = DateTime.Today.ToString();
+                Today.From = From;
+                Today.To = To;
+            }
+
+            //Check input validity. If format or range not valid, DateTime.TryParse gives 0:00
+            if (Today.From == "0:00" || Today.To == "0:00")
+            {
+                MessageBox.Show("The given time input is not supported. Please use \"hh:mm\" with 24h format.", "Wrong Time Format", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            dates.Add(Today);
+        }
+
+        public static void LoadList(List<Dates> dates)
+        {
+            //Check file
+            if (!File.Exists(path))
+                return;
+
+            //Read records
+            using (var file = new StreamReader(path))
+            {
+                string Buff;
+                while ((Buff = file.ReadLine()) != null)
+                {
+                    string[] Buffsplit = Buff.Split(' ');
+                    Dates Date = new Dates();
+                    {
+                        Date.Date = Buffsplit[0];
+                        Date.From = Buffsplit[1];
+                        Date.To = Buffsplit[2];
+                    }
+                    dates.Add(Date);
+                }
+            }
+
+        }
+
+        public static bool CheckToday(List<Dates> dates)
+        {
+            Dates LastRecord = dates.Last();
+            if (LastRecord.Date == DateTime.Today.ToShortDateString())
+                return true;
+            return false;
+        }
+
+        public static void WriteStatistics(StreamWriter file, List<Dates> DataList, double HourlyWage, double SumHours, string PayDate)
+        {
+
+            file.WriteLine("4 week summary (" + PayDate + ")\n");
+
+            file.WriteLine("    Day        From     To    Hours\n");
+            int DaysWorked = 0;
+            foreach(var date in DataList)
+            {
+
+                if (DateTime.Parse(date.Date) < DateTime.Parse(PayDate) && DateTime.Parse(date.Date) >= DateTime.Parse(PayDate).AddDays(-28))
+                {
+                    DaysWorked++;
+                    file.WriteLine(date.Date + " | " + date.From + " | " + date.To + " | " + date.Worked);
+                }
+            }
+            file.WriteLine( "\nDays worked: " + DaysWorked +
+                            "\nHours worked: " + SumHours +
+                            "\nAverage hours per workday: " + SumHours/DaysWorked +
+                            "\nAverage hours per day: " + SumHours/28 +
+                            "\nSalary: " + (SumHours * HourlyWage).ToString("0.00") + " £\n" +
+                            "Expected (taxed) salary: " + MainWindow.CalculateTax(SumHours * HourlyWage).ToString("0.00") + " £" +
+                            "\nTax amount: " + ((SumHours * HourlyWage) - MainWindow.CalculateTax(SumHours * HourlyWage)).ToString("0.00") + " £");
+            file.WriteLine("\n#########################################################################\n");
+
+
+
+        }
+
+
+    }
+}
---
+++ DominoHours/DominoHours/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Reflection;
+
+namespace DominoHours
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CreateRecord_Click(object sender, RoutedEventArgs e)
+        {
+
+                Dates.AddToList(Dates.DateList, TextBox_From.Text, TextBox_To.Text);
+            if (Dates.DateList.Count != 0)
+            {
+                Dates Today = Dates.DateList.Last();
+
+                //Add new record and refresh UI
+                Record_DataGrid.Items.Refresh();
+                RefreshStatistics(StatBox);
+                if (Dates.CheckToday(Dates.DateList))
+                {
+                    CreateRecord.IsEnabled = false;
+                }
+            }
+      
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            //Set Record_DataGrid source
+            Record_DataGrid.ItemsSource = Dates.DateList;
+
+            if (File.Exists(Dates.path))
+            {
+
+                //Set textbox date
+                TodayText.Text = "Create record for: \n" + DateTime.Today.ToShortDateString();
+                TodayText.TextAlignment = TextAlignment.Center;
+
+                //Load records from file
+                Dates.LoadList(Dates.DateList);
+
+                //Check if today is already recorded, no duplicates
+                if (Dates.CheckToday(Dates.DateList))
+                {
+                    MessageBox.Show("Today is already recorded.", "Warning", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                    CreateRecord.IsEnabled = false;
+                }
+
+                //Show statistics
+                RefreshStatistics(StatBox);
+            }
+            else
+            {
+                //to be implemented
+            }
+
+        }
+
+        public void RefreshStatistics(TextBlock StatTextBlock)
+        {
+            string FirstCutoffDate = "2018.10.22.", NextPayDate = "2018.10.26";
+            double SumHours = 0, HourlyWage = 7.83;
+
+            DateTime d2; DateTime.TryParse(FirstCutoffDate, out d2);
+
+            foreach (var date in Dates.DateList)
+            {
+                DateTime d1; DateTime.TryParse(date.Date, out d1);
+ 
+                if ((d1 - d2).TotalDays % 28 == 0)
+                {
+                    NextPayDate = d1.AddDays(32).ToShortDateString();
+                    SumHours = 0;
+                }
+                TimeSpan.TryParse(date.Worked, out TimeSpan timeSpan);
+                SumHours += timeSpan.TotalHours;
+            }
+            StatTextBlock.Text = "Statistics:\nHours worked: " + SumHours.ToString("0.00") +
+                "\nSalary: " + (SumHours * HourlyWage).ToString("0.00") + " £ on " + NextPayDate + "\n" +
+                "Expected (taxed) salary: " + CalculateTax(SumHours*HourlyWage).ToString("0.00") + " £" +
+                "\nTax amount: " + ((SumHours * HourlyWage) - CalculateTax(SumHours * HourlyWage)).ToString("0.00") + " £"; 
+
+        }
+
+        //Calculate taxed salary with 2018 data, and 20% taxrate (L1185 taxcode)
+        public static double CalculateTax(double SumInPounds)
+        {
+            double TaxedSum = 0, TaxFreeIncome, TaxedIncome, PersonalAllowance = 11850/12;
+
+            TaxFreeIncome = SumInPounds > PersonalAllowance ? PersonalAllowance : SumInPounds;
+            TaxedIncome = (SumInPounds - TaxFreeIncome) * 0.8;
+
+            TaxedSum = TaxFreeIncome + TaxedIncome;
+
+            return TaxedSum;
+        }
+
+        private void ResetRecord_Click(object sender, RoutedEventArgs e)
+        {
+            //Removes previous record and writes the new one
+            Dates.DateList.Remove(Dates.DateList.Last());
+            CreateRecord.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            //Save to file, must delete to avoid duplicates (more memory friendly than iterating through the file and removing last line)
+            string FirstCutoffDate = "2018.09.24.", NextPayDate = "2018.09.28";
+            double SumHours = 0, HourlyWage = 7.83;
+            
+            DateTime d2; DateTime.TryParse(FirstCutoffDate, out d2);
+
+            File.Delete(Dates.path);
+            using (var file = File.AppendText(Dates.path))
+            {
+                foreach (Dates date in Dates.DateList)
+                {
+                    DateTime d1; DateTime.TryParse(date.Date, out d1);
+                    file.WriteLine(date.Date + " " + date.From + " " + date.To);
+
+                    if ((d1 - d2).TotalDays % 28 == 0)
+                    {
+                        File.Delete(Dates.path_Statistics);
+                        using (var fileStat = File.AppendText(Dates.path_Statistics))
+                        {
+                            Dates.WriteStatistics(fileStat, Dates.DateList, HourlyWage, SumHours, date.Date);
+                        }
+
+                        NextPayDate = d1.AddDays(32).ToShortDateString();
+                        SumHours = 0;
+                    }
+                    TimeSpan.TryParse(date.Worked, out TimeSpan timeSpan);
+                    SumHours += timeSpan.TotalHours;
+                }
+            }
+
+        }
+    }
+}
